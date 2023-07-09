4ddcb22fbf0ebd1779ce1597d08e83523a900c1a
Howard Lee <howardleecs1@gmail.com>
2023-07-09 15:09:51 +0800

    products rework

--- products_script.js
+++ products_script.js
@@ -1,125 +1,10 @@
-/* displaying content based on the buttons clicked */
-function showFnb() {
-    var fnb = document.getElementById("fnb");
-    var health = document.getElementById("health");
-    var personal = document.getElementById("personal");
-    var hotel = document.getElementById("hotel");
-    var retail = document.getElementById("retail");
-    var sport = document.getElementById("sport");
-    var entert = document.getElementById("entert");
-    fnb.style.display = "grid";
-    health.style.display = "none";
-    personal.style.display = "none";
-    hotel.style.display = "none";
-    retail.style.display = "none";
-    sport.style.display = "none";
-    entert.style.display = "none";
-
-}
-function showHealth() {
-    var fnb = document.getElementById("fnb");
-    var health = document.getElementById("health");
-    var personal = document.getElementById("personal");
-    var hotel = document.getElementById("hotel");
-    var retail = document.getElementById("retail");
-    var sport = document.getElementById("sport");
-    var entert = document.getElementById("entert");
-    fnb.style.display = "none";
-    health.style.display = "grid";
-    personal.style.display = "none";
-    hotel.style.display = "none";
-    retail.style.display = "none";
-    sport.style.display = "none";
-    entert.style.display = "none";
-}
-function showPersonal() {
-    var fnb = document.getElementById("fnb");
-    var health = document.getElementById("health");
-    var personal = document.getElementById("personal");
-    var hotel = document.getElementById("hotel");
-    var retail = document.getElementById("retail");
-    var sport = document.getElementById("sport");
-    var entert = document.getElementById("entert");
-    fnb.style.display = "none";
-    health.style.display = "none";
-    personal.style.display = "grid";
-    hotel.style.display = "none";
-    retail.style.display = "none";
-    sport.style.display = "none";
-    entert.style.display = "none";
-}
-function showHotel() {
-    var fnb = document.getElementById("fnb");
-    var health = document.getElementById("health");
-    var personal = document.getElementById("personal");
-    var hotel = document.getElementById("hotel");
-    var retail = document.getElementById("retail");
-    var sport = document.getElementById("sport");
-    var entert = document.getElementById("entert");
-    fnb.style.display = "none";
-    health.style.display = "none";
-    personal.style.display = "none";
-    hotel.style.display = "grid";
-    retail.style.display = "none";
-    sport.style.display = "none";
-    entert.style.display = "none";
-}
-function showRetail() {
-    var fnb = document.getElementById("fnb");
-    var health = document.getElementById("health");
-    var personal = document.getElementById("personal");
-    var hotel = document.getElementById("hotel");
-    var retail = document.getElementById("retail");
-    var sport = document.getElementById("sport");
-    var entert = document.getElementById("entert");
-    fnb.style.display = "none";
-    health.style.display = "none";
-    personal.style.display = "none";
-    hotel.style.display = "none";
-    retail.style.display = "grid";
-    sport.style.display = "none";
-    entert.style.display = "none";
-}
-function showSport() {
-    var fnb = document.getElementById("fnb");
-    var health = document.getElementById("health");
-    var personal = document.getElementById("personal");
-    var hotel = document.getElementById("hotel");
-    var retail = document.getElementById("retail");
-    var sport = document.getElementById("sport");
-    var entert = document.getElementById("entert");
-    fnb.style.display = "none";
-    health.style.display = "none";
-    personal.style.display = "none";
-    hotel.style.display = "none";
-    retail.style.display = "none";
-    sport.style.display = "grid";
-    entert.style.display = "none";
-}
-function showEntert() {
-    var fnb = document.getElementById("fnb");
-    var health = document.getElementById("health");
-    var personal = document.getElementById("personal");
-    var hotel = document.getElementById("hotel");
-    var retail = document.getElementById("retail");
-    var sport = document.getElementById("sport");
-    var entert = document.getElementById("entert");
-    fnb.style.display = "none";
-    health.style.display = "none";
-    personal.style.display = "none";
-    hotel.style.display = "none";
-    retail.style.display = "none";
-    sport.style.display = "none";
-    entert.style.display = "grid";
-}
-
 /* switch active button based on button clicked */
 function activateButton(buttonId) {
   // Get the button element by its ID
   const button = document.getElementById(buttonId);
 
   // Remove the "active-btn" class from all buttons
-  const buttons = document.querySelectorAll('.category button');
+  const buttons = document.querySelectorAll('.button-container button');
   buttons.forEach((btn) => {
     btn.classList.remove('active-btn');
     btn.classList.add('not-active');
@@ -129,3 +14,20 @@ function activateButton(buttonId) {
   button.classList.remove('not-active');
   button.classList.add('active-btn');
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const buttons = document.querySelectorAll('#coupon-mart .button-container button');
+    const collections = document.querySelectorAll('#coupon-mart .merchants .collection');
+    
+    buttons.forEach((button, index) => {
+
+      button.addEventListener('click', () => {
+        collections.forEach(collection => {
+          collection.style.display = 'none';
+        });
+        collections[index].style.display = 'grid';
+      });
+    });
+
+});
